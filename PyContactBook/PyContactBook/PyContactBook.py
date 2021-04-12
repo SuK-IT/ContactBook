d7623e77ec4ca882cc922964bee365f0839afdbf
Kayory <corvex166@gmail.com>
2021-04-12 13:49:08 +0200

    fixed merge with kko.

--- PyContactBook/PyContactBook/PyContactBook.py
+++ PyContactBook/PyContactBook/PyContactBook.py
@@ -1,10 +1,6 @@
-#author Kevin Kowalski
-#SUK IT
-
 import mysql.connector
 import time
 
-<<<<<<< HEAD
 connection_config = {
         'user': 'sukit',
         'password': '10erWennDu!',
@@ -19,12 +15,9 @@ try:
     mydb = mysql.connector.connect(**connection_config)
 except:
     mydb = None
-=======
->>>>>>> 9cc9edf9a6f01b6bde42afd97c701c352fe9131f
 
 contact =[firstname, lastname, adress, phonenumber, mobilenumber, email]
 
-<<<<<<< HEAD
 def main():
     isConnected()
 
@@ -44,29 +37,17 @@ def isConnected():
 
     print("Connected to remote database" + connection_config['host'] + ":" + str(connection_config['port']) + ".")
 
-=======
-connection_config = {
-        'user': 'sukit',
-        'password': '10erWennDu!',
-        'host': 'griefed.de',
-        'database': 'contact',
-        'raise_on_warnings': True,
-        'port': 3306
-    }
-
-mydb = mysql.connector.connect(**connection_config)
->>>>>>> 9cc9edf9a6f01b6bde42afd97c701c352fe9131f
-
-
 def main():
- 
-  contact[0] = input ("Geben Sie den Vornamen ein: ")
-  contact[1] = input ("Geben Sie den Nachnamen ein: ")
-  contact[2] = input ("Geben Sie die Adresse ein: ")
-  contact[3] = input ("Geben Sie die Mobilnummer ein: ")
-  contact[4] = input ("Geben Sie die Mobilnummer ein: ")
-  contact[5] = input ("Geben Sie die Emailadresse ein: ").strip()
-  user = contact[5][:contact[5].index("@")]
-  domain = contact[5][contact[5].index("@")+1:]
-  ret = mydb.cmd_ping()
+
+    isConnected()
+
+    contact[0] = input ("Geben Sie den Vornamen ein: ")
+    contact[1] = input ("Geben Sie den Nachnamen ein: ")
+    contact[2] = input ("Geben Sie die Adresse ein: ")
+    contact[3] = input ("Geben Sie die Mobilnummer ein: ")
+    contact[4] = input ("Geben Sie die Mobilnummer ein: ")
+    contact[5] = input ("Geben Sie die Emailadresse ein: ").strip()
+    user = contact[5][:contact[5].index("@")]
+    domain = contact[5][contact[5].index("@")+1:]
+
 main()
