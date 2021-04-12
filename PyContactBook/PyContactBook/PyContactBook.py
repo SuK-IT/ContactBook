a3eefb8c3a31f388ad54c095f5d1964b930e3d47
Kayory <corvex166@gmail.com>
2021-04-12 14:01:23 +0200

    adjusted with pull from dhe

--- PyContactBook/PyContactBook/PyContactBook.py
+++ PyContactBook/PyContactBook/PyContactBook.py
@@ -1,15 +1,8 @@
-<<<<<<< HEAD
-=======
-# author Kevin Kowalski
-# SUK IT
 from errno import errorcode
-
->>>>>>> 91325b84a8e7ffa0986dc1b5558560e89484e9b8
 import mysql.connector
 import time
 
 connection_config = {
-<<<<<<< HEAD
         'user': 'sukit',
         'password': '10erWennDu!',
         'host': 'griefed.de',
@@ -19,10 +12,8 @@ connection_config = {
         'connect_timeout': 10,
     }
 
-try:
-    mydb = mysql.connector.connect(**connection_config)
-except:
-    mydb = None
+mydb = None
+cursor = None
 
 contact =[firstname, lastname, adress, phonenumber, mobilenumber, email]
 
@@ -39,32 +30,9 @@ def isConnected():
         except:
             mydb = None
 
+    cursor = mydb.cursor()
+
     print("Connected to remote database" + connection_config['host'] + ":" + str(connection_config['port']) + ".")
-
-def main():
-
-    isConnected()
-
-    contact[0] = input ("Geben Sie den Vornamen ein: ")
-    contact[1] = input ("Geben Sie den Nachnamen ein: ")
-    contact[2] = input ("Geben Sie die Adresse ein: ")
-    contact[3] = input ("Geben Sie die Mobilnummer ein: ")
-    contact[4] = input ("Geben Sie die Mobilnummer ein: ")
-    contact[5] = input ("Geben Sie die Emailadresse ein: ").strip()
-    user = contact[5][:contact[5].index("@")]
-    domain = contact[5][contact[5].index("@")+1:]
-
-=======
-    'user': 'sukit',
-    'password': '10erWennDu!',
-    'host': 'griefed.de',
-    'port': '3306',
-    'database': 'contact',
-    'raise_on_warnings': True
-}
-
-cnx = mysql.connector.connect(**connection_config)
-cursor = cnx.cursor()
 
 def create_database(cursor):
     try:
@@ -85,21 +53,32 @@ def create_database(cursor):
             print(err)
             exit(1)
 
-answer = "N"
+def create_entry():
 
-while answer != "Y":
-    name = input("Bitte geben Sie den Nachnamen des neuen Kontakts ein: ")
-    vorname = input("Bitte geben Sie den Vornamen des neuen Kontakts ein: ")
-    tel = input("Bitte geben Sie die Telefonnummer des neuen Kontakts ein: ")
-    strasse = input("Bitte geben Sie den Strassennamen des neuen Kontakts ein: ")
-    hausnr = input("Bitte geben Sie die Hausnummer des neuen Kontakts ein: ")
-    print("Der neue Kontakt lautet: ", vorname, name, tel, strasse, hausnr, " Sind sie damit einverstanden?")
-    answer = input("Bitte mit Y/N bestätigen: ")
+    answer = "N"
 
-print("Hurra", vorname, name, tel, strasse, hausnr)
+    while answer != "Y":
+        name = input("Bitte geben Sie den Nachnamen des neuen Kontakts ein: ")
+        vorname = input("Bitte geben Sie den Vornamen des neuen Kontakts ein: ")
+        tel = input("Bitte geben Sie die Telefonnummer des neuen Kontakts ein: ")
+        strasse = input("Bitte geben Sie den Strassennamen des neuen Kontakts ein: ")
+        hausnr = input("Bitte geben Sie die Hausnummer des neuen Kontakts ein: ")
+        print("Der neue Kontakt lautet: ", vorname, name, tel, strasse, hausnr, " Sind sie damit einverstanden?")
+        answer = input("Bitte mit Y/N bestätigen: ")
+
+    print("Hurra", vorname, name, tel, strasse, hausnr)
+    
+    return None
 
 def main():
-    print("Hello World")
 
->>>>>>> 91325b84a8e7ffa0986dc1b5558560e89484e9b8
-main()
+    isConnected()
+
+    contact[0] = input ("Geben Sie den Vornamen ein: ")
+    contact[1] = input ("Geben Sie den Nachnamen ein: ")
+    contact[2] = input ("Geben Sie die Adresse ein: ")
+    contact[3] = input ("Geben Sie die Mobilnummer ein: ")
+    contact[4] = input ("Geben Sie die Mobilnummer ein: ")
+    contact[5] = input ("Geben Sie die Emailadresse ein: ").strip()
+    user = contact[5][:contact[5].index("@")]
+    domain = contact[5][contact[5].index("@")+1:]
