6507d0387debff82f2bbc6a8d3b0b67c7a1fe4e5
Kayory <corvex166@gmail.com>
2021-04-12 13:02:30 +0200

    adjusted port configuration

--- PyContactBook/PyContactBook/PyContactBook.py
+++ PyContactBook/PyContactBook/PyContactBook.py
@@ -6,14 +6,17 @@ import mysql.connector
 connection_config = {
         'user': 'sukit',
         'password': '10erWennDu!',
-        'host': 'griefed.de:3306',
+        'host': 'griefed.de',
         'database': 'contact',
-        'raise_on_warnings': True
+        'raise_on_warnings': True,
+        'port': 3306
     }
 
 mydb = mysql.connector.connect(**connection_config)
 
 
 def main():
-    print("Hello World")
+    print("Hello World") 
+
+    ret = mydb.cmd_ping()
 main()
