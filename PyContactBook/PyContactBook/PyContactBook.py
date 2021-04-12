f8cf117709a6c13b29115cc01fa56b6e1707017a
Kayory <corvex166@gmail.com>
2021-04-12 16:03:29 +0200

    cleanup for pull

--- PyContactBook/PyContactBook/PyContactBook.py
+++ PyContactBook/PyContactBook/PyContactBook.py
@@ -15,9 +15,6 @@ connection_config = {
 mydb = None
 cursor = None
 
-contact = ["", "", "", "", "", ""]
-
-
 def isConnected():
 
     global mydb
@@ -34,42 +31,6 @@ def isConnected():
     cursor = mydb.cursor()
 
     print("Connected to remote database " + connection_config['host'] + ":" + str(connection_config['port']) + ".")
-
-def create_database(cursor):
-    try:
-        cursor.execute(
-            "CREATE DATABASE {} DEFAULT CHARACTER SET 'utf-8'".format(connection_config['database']))
-    except mysql.connector.Error as err:
-        print("Failed creating database: {}".format(err))
-        exit(1)
-    try:
-        cursor.execute("USE {}".format(connection_config['database']))
-    except mysql.connector.Error as err:
-        print("Database {} does not exist.".format(connection_config['database']))
-        if err.errno == errorcode.ER_BAD_DB_ERROR:
-            create_database(cursor)
-            print("Database {} created successfully.".format(connection_config['database']))
-            cnx.database = connection_config['database']
-        else:
-            print(err)
-            exit(1)
-
-def create_entry():
-
-    answer = "N"
-
-    while answer != "Y":
-        name = input("Bitte geben Sie den Nachnamen des neuen Kontakts ein: ")
-        vorname = input("Bitte geben Sie den Vornamen des neuen Kontakts ein: ")
-        tel = input("Bitte geben Sie die Telefonnummer des neuen Kontakts ein: ")
-        strasse = input("Bitte geben Sie den Strassennamen des neuen Kontakts ein: ")
-        hausnr = input("Bitte geben Sie die Hausnummer des neuen Kontakts ein: ")
-        print("Der neue Kontakt lautet: ", vorname, name, tel, strasse, hausnr, " Sind sie damit einverstanden?")
-        answer = input("Bitte mit Y/N bestätigen: ")
-
-    print("Hurra", vorname, name, tel, strasse, hausnr)
-    
-    return None
 
 
 def create_entry_dict():
@@ -118,13 +79,5 @@ def main():
 
     print(create_entry_dict()['surname'])
 
-    #contact[0] = input ("Geben Sie den Vornamen ein: ")
-    #contact[1] = input ("Geben Sie den Nachnamen ein: ")
-    #contact[2] = input ("Geben Sie die Adresse ein: ")
-    #contact[3] = input ("Geben Sie die Mobilnummer ein: ")
-    #contact[4] = input ("Geben Sie die Mobilnummer ein: ")
-    #contact[5] = input ("Geben Sie die Emailadresse ein: ").strip()
-    #user = contact[5][:contact[5].index("@")]
-    #domain = contact[5][contact[5].index("@")+1:]
 
 main()
