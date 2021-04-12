9cc9edf9a6f01b6bde42afd97c701c352fe9131f
Afaryz <afaryz33@gmail.com>
2021-04-12 13:45:43 +0200

    no message

--- PyContactBook/PyContactBook/PyContactBook.py
+++ PyContactBook/PyContactBook/PyContactBook.py
@@ -3,14 +3,30 @@
 
 import mysql.connector
 
-mydb = mysql.connector.connect(
-  host="localhost",
-  user="yourusername",
-  password="yourpassword"
-)
+
+contact =[firstname, lastname, adress, phonenumber, mobilenumber, email]
+
+connection_config = {
+        'user': 'sukit',
+        'password': '10erWennDu!',
+        'host': 'griefed.de',
+        'database': 'contact',
+        'raise_on_warnings': True,
+        'port': 3306
+    }
+
+mydb = mysql.connector.connect(**connection_config)
 
 
 def main():
-    print("Hello World")
-
+ 
+  contact[0] = input ("Geben Sie den Vornamen ein: ")
+  contact[1] = input ("Geben Sie den Nachnamen ein: ")
+  contact[2] = input ("Geben Sie die Adresse ein: ")
+  contact[3] = input ("Geben Sie die Mobilnummer ein: ")
+  contact[4] = input ("Geben Sie die Mobilnummer ein: ")
+  contact[5] = input ("Geben Sie die Emailadresse ein: ").strip()
+  user = contact[5][:contact[5].index("@")]
+  domain = contact[5][contact[5].index("@")+1:]
+  ret = mydb.cmd_ping()
 main()
